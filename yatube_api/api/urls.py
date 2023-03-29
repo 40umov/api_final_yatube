de1b6db41d2ca193e79a70d7c78215ce8b3c212c
Evgeniy Sorokoumov <40umov.evg@gmail.com>
2023-03-29 21:08:18 +0300

    Change yatube_api/api/urls.py, router vs router_v1.

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -3,19 +3,19 @@ from rest_framework.routers import SimpleRouter
 
 from .views import CommentViewSet, FollowViewSet, GroupViewSet, PostViewSet
 
-router = SimpleRouter()
+router_v1 = SimpleRouter()
 
-router.register('posts', PostViewSet)
-router.register(r'posts/(?P<post_id>\d+)/comments',
-                CommentViewSet,
-                basename='comments')
-router.register('groups', GroupViewSet)
-router.register('follow',
-                FollowViewSet,
-                basename='follow')
+router_v1.register('posts', PostViewSet)
+router_v1.register(r'posts/(?P<post_id>\d+)/comments',
+                   CommentViewSet,
+                   basename='comments')
+router_v1.register('groups', GroupViewSet)
+router_v1.register('follow',
+                   FollowViewSet,
+                   basename='follow')
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('', include(router_v1.urls)),
     path('', include('djoser.urls')),
     path('', include('djoser.urls.jwt'))
 ]
